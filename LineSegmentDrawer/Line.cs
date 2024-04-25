87ad8bacde6fac8d0395b2a19ce3d4baf60a391b
clavinsh <klavins.ar@gmail.com>
2024-04-25 16:39:33 +0300

    Add project files.

---
+++ LineSegmentDrawer/Line.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+
+namespace LineSegmentDrawer
+{
+    public class Line(Color lineColor, Point start, Point end)
+    {
+        public Bitmap CreateImage(int imageWidth, int imageHeight)
+        {
+            Bitmap bmp = new(imageWidth, imageHeight);
+            Graphics g = Graphics.FromImage(bmp);
+
+            DrawLine(g, start.X, start.Y, end.X, end.Y, lineColor);
+
+            return bmp;
+        }
+
+        private static void DrawLine(Graphics g, int x0, int y0, int x1, int y1, Color color)
+        {
+            float dx = x1 - x0;
+            float dy = y1 - y0;
+
+            float gradient = dy / dx;
+
+            float yStep = y0 + gradient;
+
+            for(int x = x0; x < x1; x++)
+            {
+                g.FillRectangle(new SolidBrush(color), x, (int)yStep, 1, 1);
+
+                yStep += gradient;
+            }
+        }
+    }
+}
---
+++ LineSegmentDrawer/Program.cs
@@ -0,0 +1,16 @@
+﻿using LineSegmentDrawer;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+Color lineColor = Color.White;
+Point start = new Point(0, 0);
+Point end = new Point(100, 100);
+
+Line line = new Line(lineColor, start, end);
+
+Console.WriteLine("Creating the image based on the supplied line parameters...");
+
+var img = line.CreateImage(200,200);
+img.Save("output.png", ImageFormat.Png);
+
+Console.WriteLine("Image created and saved!");
