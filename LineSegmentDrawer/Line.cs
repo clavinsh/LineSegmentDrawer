8f56145ffc5d3bd775010b980d891f362476a98e
clavinsh <klavins.ar@gmail.com>
2024-04-27 15:17:16 +0300

    Added color blending with line and background

--- LineSegmentDrawer/Line.cs
+++ LineSegmentDrawer/Line.cs
@@ -1,46 +1,16 @@
 ﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Reflection.Metadata.Ecma335;
 
 namespace LineSegmentDrawer
 {
-    public class Line
+    public class Line(double x0, double y0, double x1, double y1, Color color)
     {
-        private double x0, y0, x1, y1;
-        private readonly Color foreColor;
-
-        public Line(double x0, double y0, double x1, double y1, Color color)
-        {
-            this.x0 = x0;
-            this.y0 = y0;
-            this.y1 = y1;
-            this.x1 = x1;
-
-            this.foreColor = color;
-        }
-
-        private void Plot(Bitmap bitmap, double x, double y, double c)
-        {
-            int alpha = (int)(c * 255);
-            if (alpha > 255) alpha = 255;
-            if (alpha < 0) alpha = 0;
-            Color color = Color.FromArgb(alpha, foreColor);
-            bitmap.SetPixel((int)x, (int)y, color);
-        }
-
-        int IntegerPart(double x) { return (int)x; }
-
-        int Round(double x) { return IntegerPart(x + 0.5); }
-
-        double FractionalPart(double x)
-        {
-            if (x < 0) return (1 - (x - Math.Floor(x)));
-            return (x - Math.Floor(x));
-        }
-
-        double InverseFractionalPart(double x)
-        {
-            return 1 - FractionalPart(x);
-        }
-
+        private double x0 = x0,
+            y0 = y0,
+            x1 = x1,
+            y1 = y1;
+        private readonly Color foreColor = color;
 
         public void Draw(Bitmap bitmap)
         {
@@ -48,13 +18,21 @@ namespace LineSegmentDrawer
             double temp;
             if (steep)
             {
-                temp = x0; x0 = y0; y0 = temp;
-                temp = x1; x1 = y1; y1 = temp;
+                temp = x0;
+                x0 = y0;
+                y0 = temp;
+                temp = x1;
+                x1 = y1;
+                y1 = temp;
             }
             if (x0 > x1)
             {
-                temp = x0; x0 = x1; x1 = temp;
-                temp = y0; y0 = y1; y1 = temp;
+                temp = x0;
+                x0 = x1;
+                x1 = temp;
+                temp = y0;
+                y0 = y1;
+                y1 = temp;
             }
 
             double dx = x1 - x0;
@@ -69,13 +47,13 @@ namespace LineSegmentDrawer
 
             if (steep)
             {
-                Plot(bitmap, yPixel1, xPixel1, InverseFractionalPart(yEnd) * xGap);
-                Plot(bitmap, yPixel1 + 1, xPixel1, FractionalPart(yEnd) * xGap);
+                DrawPixel(bitmap, yPixel1, xPixel1, InverseFractionalPart(yEnd) * xGap);
+                DrawPixel(bitmap, yPixel1 + 1, xPixel1, FractionalPart(yEnd) * xGap);
             }
             else
             {
-                Plot(bitmap, xPixel1, yPixel1, InverseFractionalPart(yEnd) * xGap);
-                Plot(bitmap, xPixel1, yPixel1 + 1, FractionalPart(yEnd) * xGap);
+                DrawPixel(bitmap, xPixel1, yPixel1, InverseFractionalPart(yEnd) * xGap);
+                DrawPixel(bitmap, xPixel1, yPixel1 + 1, FractionalPart(yEnd) * xGap);
             }
             double yIntersection = yEnd + gradient;
 
@@ -86,21 +64,31 @@ namespace LineSegmentDrawer
             double yPixel2 = IntegerPart(yEnd);
             if (steep)
             {
-                Plot(bitmap, yPixel2, xPixel2, InverseFractionalPart(yEnd) * xGap);
-                Plot(bitmap, yPixel2 + 1, xPixel2, FractionalPart(yEnd) * xGap);
+                DrawPixel(bitmap, yPixel2, xPixel2, InverseFractionalPart(yEnd) * xGap);
+                DrawPixel(bitmap, yPixel2 + 1, xPixel2, FractionalPart(yEnd) * xGap);
             }
             else
             {
-                Plot(bitmap, xPixel2, yPixel2, InverseFractionalPart(yEnd) * xGap);
-                Plot(bitmap, xPixel2, yPixel2 + 1, FractionalPart(yEnd) * xGap);
+                DrawPixel(bitmap, xPixel2, yPixel2, InverseFractionalPart(yEnd) * xGap);
+                DrawPixel(bitmap, xPixel2, yPixel2 + 1, FractionalPart(yEnd) * xGap);
             }
 
             if (steep)
             {
                 for (int x = (int)(xPixel1 + 1); x <= xPixel2 - 1; x++)
                 {
-                    Plot(bitmap, IntegerPart(yIntersection), x, InverseFractionalPart(yIntersection));
-                    Plot(bitmap, IntegerPart(yIntersection) + 1, x, FractionalPart(yIntersection));
+                    DrawPixel(
+                        bitmap,
+                        IntegerPart(yIntersection),
+                        x,
+                        InverseFractionalPart(yIntersection)
+                    );
+                    DrawPixel(
+                        bitmap,
+                        IntegerPart(yIntersection) + 1,
+                        x,
+                        FractionalPart(yIntersection)
+                    );
                     yIntersection += gradient;
                 }
             }
@@ -108,11 +96,86 @@ namespace LineSegmentDrawer
             {
                 for (int x = (int)(xPixel1 + 1); x <= xPixel2 - 1; x++)
                 {
-                    Plot(bitmap, x, IntegerPart(yIntersection), InverseFractionalPart(yIntersection));
-                    Plot(bitmap, x, IntegerPart(yIntersection) + 1, FractionalPart(yIntersection));
+                    DrawPixel(
+                        bitmap,
+                        x,
+                        IntegerPart(yIntersection),
+                        InverseFractionalPart(yIntersection)
+                    );
+                    DrawPixel(
+                        bitmap,
+                        x,
+                        IntegerPart(yIntersection) + 1,
+                        FractionalPart(yIntersection)
+                    );
                     yIntersection += gradient;
                 }
             }
+        }
+
+        private void DrawPixel(Bitmap bitmap, double x, double y, double c)
+        {
+            int alpha = (int)(c * 255);
+            Math.Clamp(alpha, 0, 255);
+
+            Color linePixel = Color.FromArgb(alpha, foreColor);
+            Color background = bitmap.GetPixel((int)x, (int)y);
+
+            Color blended = BlendColors(linePixel, background);
+
+            bitmap.SetPixel((int)x, (int)y, blended);
+        }
+
+        // from https://en.wikipedia.org/wiki/Alpha_compositing
+        private static Color BlendColors(Color foreground, Color background)
+        {
+            float foregroundAlpha = foreground.A / 255f;
+            float backgroundAlpha = background.A / 255f;
+
+            float blendAlpha = foregroundAlpha + backgroundAlpha * (1 - foregroundAlpha);
+
+            int blendRed = (int)(
+                (
+                    foreground.R * foregroundAlpha
+                    + background.R * backgroundAlpha * (1 - foregroundAlpha)
+                ) / blendAlpha
+            );
+            int blendGreen = (int)(
+                (
+                    foreground.G * foregroundAlpha
+                    + background.G * backgroundAlpha * (1 - foregroundAlpha)
+                ) / blendAlpha
+            );
+            int blendBlue = (int)(
+                (
+                    foreground.B * foregroundAlpha
+                    + background.B * backgroundAlpha * (1 - foregroundAlpha)
+                ) / blendAlpha
+            );
+
+            return Color.FromArgb((int)(blendAlpha * 255f), blendRed, blendGreen, blendBlue);
+        }
+
+        private static int IntegerPart(double x)
+        {
+            return (int)x;
+        }
+
+        private static int Round(double x)
+        {
+            return IntegerPart(x + 0.5);
+        }
+
+        private static double FractionalPart(double x)
+        {
+            if (x < 0)
+                return (1 - (x - Math.Floor(x)));
+            return (x - Math.Floor(x));
+        }
+
+        private static double InverseFractionalPart(double x)
+        {
+            return 1 - FractionalPart(x);
         }
     }
 }
--- LineSegmentDrawer/Program.cs
+++ LineSegmentDrawer/Program.cs
@@ -1,6 +1,6 @@
-﻿using LineSegmentDrawer;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Drawing.Imaging;
+using LineSegmentDrawer;
 
 Color lineColor = Color.Green;
 Color backgroundColor = Color.DarkSlateBlue;
@@ -10,11 +10,15 @@ Point end = new Point(50, 173);
 //Line line = new Line(lineColor, backgroundColor, start.X, start.Y, end.X, end.Y);
 
 
-Line line = new Line(start.X, start.Y, end.X,end.Y, lineColor);
+Line line = new Line(start.X, start.Y, end.X, end.Y, lineColor);
 
 Console.WriteLine("Creating the image based on the supplied line parameters...");
 
 var image = new Bitmap(200, 200);
+Graphics g = Graphics.FromImage(image);
+
+g.Clear(backgroundColor);
+
 line.Draw(image);
 image.Save("output.png", ImageFormat.Png);
 
