102c84468e2aac7e0de2f5276e27671490a7e905
clavinsh <klavins.ar@gmail.com>
2024-04-27 12:45:36 +0300

    Updated Wu' alg implementation from Rossetta Code

--- LineSegmentDrawer/Line.cs
+++ LineSegmentDrawer/Line.cs
@@ -2,208 +2,117 @@
 
 namespace LineSegmentDrawer
 {
-    public class Line(Color lineColor, Color backgroundColor, double x0, double y0, double x1, double y1)
+    public class Line
     {
-        public Bitmap CreateImage(int imageWidth, int imageHeight)
+        private double x0, y0, x1, y1;
+        private readonly Color foreColor;
+
+        public Line(double x0, double y0, double x1, double y1, Color color)
         {
-            Bitmap bmp = new(imageWidth, imageHeight);
-            Graphics g = Graphics.FromImage(bmp);
+            this.x0 = x0;
+            this.y0 = y0;
+            this.y1 = y1;
+            this.x1 = x1;
 
-            //g.Clear(backgroundColor);
-
-            DrawLineWu(g, x0, y0, x1, y1, lineColor);
-
-            return bmp;
+            this.foreColor = color;
         }
 
-        //line drawing with anti-aliasing using Xiaolin Wu method
-        private static void DrawLineWu(Graphics g, double x0, double y0, double x1, double y1, Color color)
+        private void Plot(Bitmap bitmap, double x, double y, double c)
+        {
+            int alpha = (int)(c * 255);
+            if (alpha > 255) alpha = 255;
+            if (alpha < 0) alpha = 0;
+            Color color = Color.FromArgb(alpha, foreColor);
+            bitmap.SetPixel((int)x, (int)y, color);
+        }
+
+        int IntegerPart(double x) { return (int)x; }
+
+        int Round(double x) { return IntegerPart(x + 0.5); }
+
+        double FractionalPart(double x)
+        {
+            if (x < 0) return (1 - (x - Math.Floor(x)));
+            return (x - Math.Floor(x));
+        }
+
+        double InverseFractionalPart(double x)
+        {
+            return 1 - FractionalPart(x);
+        }
+
+
+        public void Draw(Bitmap bitmap)
         {
             bool steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
-
+            double temp;
             if (steep)
             {
-                Swap(ref x0, ref y0);
-                Swap(ref x1, ref y1);
+                temp = x0; x0 = y0; y0 = temp;
+                temp = x1; x1 = y1; y1 = temp;
             }
-
             if (x0 > x1)
             {
-                Swap(ref x0, ref x1);
-                Swap(ref y0, ref y1);
+                temp = x0; x0 = x1; x1 = temp;
+                temp = y0; y0 = y1; y1 = temp;
             }
 
             double dx = x1 - x0;
             double dy = y1 - y0;
+            double gradient = dy / dx;
 
-            double gradient;
-            if (dx == 0.0f)
-            {
-                gradient = 1.0f;
-            }
-            else
-            {
-                gradient = dy / dx;
-            }
-
-            // first end point
-            double xEnd = Math.Round(x0, 0, MidpointRounding.AwayFromZero);
+            double xEnd = Round(x0);
             double yEnd = y0 + gradient * (xEnd - x0);
-
             double xGap = InverseFractionalPart(x0 + 0.5);
-
             double xPixel1 = xEnd;
-            double yPixel1 = (int)yEnd;
+            double yPixel1 = IntegerPart(yEnd);
 
             if (steep)
             {
-                PlotPixel(g, (int)yPixel1, (int)xPixel1, InverseFractionalPart(yEnd) * xGap, color);
-                PlotPixel(g, (int)yPixel1 + 1, (int)xPixel1, FractionalPart(yEnd) * xGap, color);
+                Plot(bitmap, yPixel1, xPixel1, InverseFractionalPart(yEnd) * xGap);
+                Plot(bitmap, yPixel1 + 1, xPixel1, FractionalPart(yEnd) * xGap);
             }
             else
             {
-                PlotPixel(g, (int)xPixel1, (int)yPixel1, InverseFractionalPart(yEnd) * xGap, color);
-                PlotPixel(g, (int)xPixel1, (int)yPixel1 + 1, FractionalPart(yEnd) * xGap, color);
+                Plot(bitmap, xPixel1, yPixel1, InverseFractionalPart(yEnd) * xGap);
+                Plot(bitmap, xPixel1, yPixel1 + 1, FractionalPart(yEnd) * xGap);
             }
+            double yIntersection = yEnd + gradient;
 
-            double intersectionY = yEnd + gradient;
-
-            //second end point
-            xEnd = Math.Round(x1, 0, MidpointRounding.AwayFromZero);
+            xEnd = Round(x1);
             yEnd = y1 + gradient * (xEnd - x1);
-
             xGap = FractionalPart(x1 + 0.5);
-
             double xPixel2 = xEnd;
-            double yPixel2 = (int)yEnd;
-
+            double yPixel2 = IntegerPart(yEnd);
             if (steep)
             {
-                PlotPixel(g, (int)yPixel2, (int)xPixel2, InverseFractionalPart(yEnd) * xGap, color);
-                PlotPixel(g, (int)yPixel2 + 1, (int)xPixel2, FractionalPart(yEnd) * xGap, color);
+                Plot(bitmap, yPixel2, xPixel2, InverseFractionalPart(yEnd) * xGap);
+                Plot(bitmap, yPixel2 + 1, xPixel2, FractionalPart(yEnd) * xGap);
             }
             else
             {
-                PlotPixel(g, (int)xPixel2, (int)yPixel2, InverseFractionalPart(yEnd) * xGap, color);
-                PlotPixel(g, (int)xPixel2, (int)yPixel2 + 1, FractionalPart(yEnd) * xGap, color);
+                Plot(bitmap, xPixel2, yPixel2, InverseFractionalPart(yEnd) * xGap);
+                Plot(bitmap, xPixel2, yPixel2 + 1, FractionalPart(yEnd) * xGap);
             }
 
-            // main loop
             if (steep)
             {
-                for (int x = (int)xPixel1 + 1; x < (int)xPixel2 - 1; x++)
+                for (int x = (int)(xPixel1 + 1); x <= xPixel2 - 1; x++)
                 {
-                    PlotPixel(g, (int)intersectionY, x, InverseFractionalPart(intersectionY), color);
-                    PlotPixel(g, (int)intersectionY + 1, x, FractionalPart(intersectionY), color);
-                    intersectionY += gradient;
+                    Plot(bitmap, IntegerPart(yIntersection), x, InverseFractionalPart(yIntersection));
+                    Plot(bitmap, IntegerPart(yIntersection) + 1, x, FractionalPart(yIntersection));
+                    yIntersection += gradient;
                 }
             }
             else
             {
-                for (int x = (int)xPixel1 + 1; x < (int)xPixel2 - 1; x++)
+                for (int x = (int)(xPixel1 + 1); x <= xPixel2 - 1; x++)
                 {
-                    PlotPixel(g, x, (int)intersectionY, InverseFractionalPart(intersectionY), color);
-                    PlotPixel(g, x, (int)intersectionY + 1, FractionalPart(intersectionY), color);
-                    intersectionY += gradient;
+                    Plot(bitmap, x, IntegerPart(yIntersection), InverseFractionalPart(yIntersection));
+                    Plot(bitmap, x, IntegerPart(yIntersection) + 1, FractionalPart(yIntersection));
+                    yIntersection += gradient;
                 }
             }
-        }
-
-        static double FractionalPart(double x) => x - Math.Floor(x);
-        static double InverseFractionalPart(double x) => 1 - FractionalPart(x);
-
-        private static void Swap(ref double a, ref double b)
-        {
-            (b, a) = (a, b);
-        }
-
-        private static void PlotPixel(Graphics g, int x, int y, double brightness, Color pixelColor)
-        {
-            RGB2HSL(pixelColor, out float hue, out float sat, out float colorBrightness);
-            colorBrightness *= (float)brightness;
-            colorBrightness = 1 - colorBrightness;
-            Color brightnessAdjusted = HSL2RGB(hue, sat, colorBrightness);
-
-            DrawPixel(g, x, y, brightnessAdjusted);
-        }
-
-        private static void DrawPixel(Graphics g, int x, int y, Color color)
-        {
-            g.FillRectangle(new SolidBrush(color), x, y, 1, 1);
-        }
-
-        private static void RGB2HSL(Color color, out float hue, out float sat, out float brightness)
-        {
-            hue = color.GetHue();
-            sat = color.GetSaturation();
-            brightness = color.GetBrightness();
-        }
-
-        //based on https://geekymonkey.com/Programming/CSharp/RGB2HSL_HSL2RGB.htm
-        public static Color HSL2RGB(double h, double sl, double l)
-        {
-            double v;
-            double r, g, b;
-
-            r = l;   // default to gray
-            g = l;
-            b = l;
-            v = (l <= 0.5) ? (l * (1.0 + sl)) : (l + sl - l * sl);
-
-            if (v > 0)
-            {
-                double m;
-                double sv;
-                int sextant;
-                double fract, vsf, mid1, mid2;
-
-                m = l + l - v;
-                sv = (v - m) / v;
-                h *= 6.0;
-
-                sextant = (int)h;
-                fract = h - sextant;
-                vsf = v * sv * fract;
-                mid1 = m + vsf;
-                mid2 = v - vsf;
-
-                switch (sextant)
-                {
-                    case 0:
-                        r = v;
-                        g = mid1;
-                        b = m;
-                        break;
-                    case 1:
-                        r = mid2;
-                        g = v;
-                        b = m;
-                        break;
-
-                    case 2:
-                        r = m;
-                        g = v;
-                        b = mid1;
-                        break;
-                    case 3:
-                        r = m;
-                        g = mid2;
-                        b = v;
-                        break;
-                    case 4:
-                        r = mid1;
-                        g = m;
-                        b = v;
-                        break;
-                    case 5:
-                        r = v;
-                        g = m;
-                        b = mid2;
-                        break;
-                }
-            }
-
-            return Color.FromArgb(Convert.ToByte(r * 255.0f), Convert.ToByte(g * 255.0f), Convert.ToByte(b * 255.0f)); ;
         }
     }
 }
--- LineSegmentDrawer/Program.cs
+++ LineSegmentDrawer/Program.cs
@@ -2,16 +2,20 @@
 using System.Drawing;
 using System.Drawing.Imaging;
 
-Color lineColor = Color.White;
+Color lineColor = Color.Green;
 Color backgroundColor = Color.DarkSlateBlue;
 Point start = new Point(10, 10);
 Point end = new Point(50, 173);
 
-Line line = new Line(lineColor, backgroundColor, start.X, start.Y, end.X, end.Y);
+//Line line = new Line(lineColor, backgroundColor, start.X, start.Y, end.X, end.Y);
+
+
+Line line = new Line(start.X, start.Y, end.X,end.Y, lineColor);
 
 Console.WriteLine("Creating the image based on the supplied line parameters...");
 
-var img = line.CreateImage(200, 200);
-img.Save("output.png", ImageFormat.Png);
+var image = new Bitmap(200, 200);
+line.Draw(image);
+image.Save("output.png", ImageFormat.Png);
 
 Console.WriteLine("Image created and saved!");
